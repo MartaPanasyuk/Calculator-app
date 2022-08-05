994937563758d11af36d0e0b4fcbe77c4a948f9e
MartaPanasyuk <mart.panasyuk@gmail.com>
2022-08-05 14:34:16 +0200

    adding buttons with the numbers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
-import logo from "./logo.svg";
 import "./App.css";
 
+const createNumber = () => {
+  let numbers = [];
+  for (let i = 1; i < 10; i++) {
+    numbers.push(<button key={i}>{i}</button>);
+  }
+  return numbers;
+};
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="app-wraper">
+        <div className="display-wrapper">
+          <span>(0)</span>0
+        </div>
+        <div className="operators">
+          <button>/</button>
+          <button>*</button>
+          <button>+</button>
+          <button>-</button>
+          <button>DEL</button>
+        </div>
+        <div className="digits">
+          {createNumber()}
+          <button>.</button>
+          <button>0</button>
+          <button>=</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
